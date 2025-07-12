0d26f945adbf16c6ac3a135d2b3cfe1b2a26bf17
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-07-12 12:31:43 +0300

    L-488, 489. Create meetup detail component that displays some dummy details. Navigate to it with useRouter.

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail.js";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://res.cloudinary.com/djcyhbk2e/image/upload/c_limit,f_auto,q_50,w_1400/v1/gvv/prod/ssuc27lzmy95cx7o8cgg"
+      title="A First Meetup"
+      address="Some address 5, 12345 Some City"
+      description="This is a first meetup!"
+    />
+  );
+}
+
+export default MeetupDetails;
