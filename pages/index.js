74b4a184dc8cacd09c2aa59cf105ae8f3eb16d43
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-07-17 20:40:01 +0300

    L-493. Showcase getServerSideProps function

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,7 @@ function HomePage(props) {
 
 /**
  * This works only in pages!
+ * Is called during build.
  * It has to be called getStaticProps so that Next.js can recognise it and execute it before rendering the page.
  * We can write any server code since it will be executed in server.
  * @returns an object with a props property
@@ -37,5 +38,19 @@ export async function getStaticProps() {
     revalidate: 1, //revalidate in seconds
   };
 }
+
+/**
+ * Similar with getStaticProps but is called after deployment.
+ */
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   };
+// }
 
 export default HomePage;
