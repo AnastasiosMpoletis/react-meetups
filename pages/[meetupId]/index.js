bf9f6d0661f7f59bfafcceefa110605625564bb6
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-07-17 20:58:10 +0300

    L-494, 495. Implement getStaticPaths, getStaticProps for dynamic meetups pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,42 @@ function MeetupDetails() {
   );
 }
 
+/**
+ * GetStaticProps needs to pre-generate all dynamic pages.
+ * Here we can define the ids.
+ */
+export async function getStaticPaths() {
+  return {
+    fallback: true, //false = all possible id values are defined here. True = only some id values are defined here
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image: "https://res.cloudinary.com/djcyhbk2e/image/upload/c_limit,f_auto,q_50,w_1400/v1/gvv/prod/ssuc27lzmy95cx7o8cgg",
+        id: meetupId,
+        title: "A First Meetup",
+        address: "Some address 5, 12345 Some City",
+        description: "This is a first meetup!",
+      }
+    }
+  };
+}
+
 export default MeetupDetails;
