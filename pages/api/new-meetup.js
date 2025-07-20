9645dcd985fc92fe94686f4871f863751acaf4c2
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-07-20 12:12:56 +0300

    L-498. Implement new meetup insert in NewMeetupPage. Navigate back when insert is completed

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -7,21 +7,21 @@ import { MongoClient } from "mongodb";
 
 async function handler(req, res) {
   if (req.method === 'POST') {
+    const client = await MongoClient.connect(process.env.MONGO_URI);
     const data = req.body;
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
-
-  const client = await MongoClient.connect(process.env.MONGO_URI)
-  const db = client.db();
-
-  const meetupsCollection = db.collection('meetups');
-
-  const result = await meetupsCollection.insertOne(data);
-
-  console.log(result);
-
-  client.close();
-
-  res.status(201).json({ message: 'Meetup inserted!' });
 }
 
 export default handler;
